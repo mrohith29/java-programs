830a6b7b0bc8d722f3880601979c8d09dba6b669
mrohith29 <mariyalarohith29@gmail.com>
2023-12-07 17:47:53 +0530

    Add new file to git

--- moodle_programs/largest_power_of_3.java
+++ moodle_programs/largest_power_of_3.java
@@ -1,2 +1,13 @@
-package moodle_programs;public class largest_power_of_3 {
+package moodle_programs;
+import java.util.Scanner;
+public class largest_power_of_3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        int po3 = 1; // since least power is 3^0 = 1
+        while (po3 <= input) {
+            po3 = po3 * 3;
+        }
+        System.out.println("The power of 3 less than the x is = " + po3/3);
+    }
 }
--- moodle_programs/reverse_2_digit_number.java
+++ moodle_programs/reverse_2_digit_number.java
@@ -1,2 +1,28 @@
-package moodle_programs;public class reverse_2_digit_number {
+package moodle_programs;
+import java.util.Scanner;
+public class reverse_2_digit_number {
+    static String  reverse(int num) {
+        String output = "";
+        int temp;
+        temp = num;
+        if (num < 0) {
+            num = -1 * num;
+        }
+        String s = String.format("%d", num);
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char ch = s.charAt(i);
+            output += ch;
+        }
+        if (temp < 0) {
+            return '-' + output;
+        }
+        else {
+            return (output);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+        System.out.println(reverse(input));
+    }
 }
