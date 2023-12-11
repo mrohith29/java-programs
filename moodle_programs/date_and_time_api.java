86f0ff83a5441136844bddc153c97d4045827e32
mrohith29 <mariyalarohith29@gmail.com>
2023-12-11 20:43:35 +0530

    new files

---
+++ moodle_programs/date_and_time_api.java
@@ -0,0 +1,15 @@
+package moodle_programs;
+import java.time.*;
+import java.time.temporal.TemporalAdjuster;
+
+public class date_and_time_api {
+    public static void main(String[] args) {
+//        System.out.println(time LocalDate());
+        LocalDate today = LocalDate.now();
+        System.out.println(today);
+//        LocalDate paydate = today.with(TemporalAdjuster.()).minusDays(2);
+//        System.out.println(paydate);
+//        MonthDay md = MonthDay.f
+
+    }
+}
