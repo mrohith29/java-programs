5aadbfd9d3dedda9312346956847b3681b44034a
mrohith29 <mariyalarohith29@gmail.com>
2023-12-17 18:00:57 +0530

    new files add

---
+++ moodle_programs/letsHit6.java
@@ -0,0 +1,10 @@
+package moodle_programs;
+import java.util.Scanner;
+public class letsHit6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(a + b);
+    }
+}
