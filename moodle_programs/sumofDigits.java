2d786db96e65068e712eb68010b0bee62e753da2
mrohith29 <mariyalarohith29@gmail.com>
2023-12-24 10:21:49 +0530

    add new files

--- moodle_programs/sumofDigits.java
+++ moodle_programs/sumofDigits.java
@@ -1,18 +1,17 @@
-package moodle_programs;
-
 import java.util.Scanner;
 
 class sumofDigits {
- public static void main(String[] args){
-   Scanner sc = new Scanner(System.in);
-   String str = sc.nextLine();
-   String []newstr = str.split("[a-zA-Z]+");
-int sum = 0;
-     for (String i :
-             newstr) {
-        int x = Integer.parseInt(i);
-        sum+=x;
-     }
-     System.out.println(sum);
-  }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String[] newstr = str.split("");
+        int sum = 0;
+        for (String i : newstr) {
+            if (Character.isDigit(i.charAt(0))) {
+                int x = Integer.parseInt(i);
+                sum += x;
+            }
+        }
+        System.out.println(sum);
+    }
 }
